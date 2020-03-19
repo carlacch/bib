f168a92b858b6fd53fbd60fc75b7053e3b40567e
Carla Cauche <58265171+carlacch@users.noreply.github.com>
2020-03-19 16:29:56 +0100

    feat(michelin): deleting accents of name and adress

--- server/michelin.js
+++ server/michelin.js
@@ -12,30 +12,30 @@ const fs = require('fs');
 const parse = data => {
   const $ = cheerio.load(data);
   const name = $('.section-main h2.restaurant-details__heading--title').text();
-  var experience = $('#experience-section > ul > li:nth-child(2)').text();
+  let experience = $('#experience-section > ul > li:nth-child(2)').text();
   experience = experience.replace('ò\n','').trim();
   return {name, experience};
 };
 
 const parseInfo = data =>{
   const $ = cheerio.load(data);
-  const name = $('.section-main h2.restaurant-details__heading--title').text();
-  var list = $('.section-main li.restaurant-details__heading-price').text().trim().split('\n');
+  const name = $('.section-main h2.restaurant-details__heading--title').text().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  let list = $('.section-main li.restaurant-details__heading-price').text().trim().split('\n');
   let type = list.pop().replace('•','').trim();
   let price ="";
   list.forEach(e => {
     price+=e.trim();
   })
-  var experience = $('#experience-section > ul > li:nth-child(2)').text();
+  let experience = $('#experience-section > ul > li:nth-child(2)').text();
   experience = experience.replace('ò\n','').replace('ó','').replace('ô','').trim();
-  var adress = $('.section-main ul.restaurant-details__heading--list li:nth-child(1)');
+  let adress = $('.section-main ul.restaurant-details__heading--list li:nth-child(1)');
   //console.log(adress.attr('class'));
   if (adress.hasClass('restaurant-details__heading-experience-title')){
     adress = adress.next('li');
     //console.log('next');
     //console.log(adress);
   }
-  adress = adress.text().trim();
+  adress = adress.text().trim().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
   const tel = $('.section-main .collapse__block-item .d-flex span.flex-fill').first().text();
   const link = $('.section-main .link-item .d-flex span.flex-fill').text();
   return {name, adress, price, type, experience,  tel, link};
@@ -76,11 +76,11 @@ module.exports.get = async (url) => {
   const resp = await axios(url);
   if (resp.status >= 200 && resp.status < 300){
     const $ = cheerio.load(resp.data);
-    var links =[];
-    var restaurants=[];
+    let links =[];
+    let restaurants=[];
 
     $('div.col-md-6.col-lg-6.col-xl-3').each(function(i, element){
-      var link = 'https://guide.michelin.com'+$(element).find('a.link').attr('href');
+      let link = 'https://guide.michelin.com'+$(element).find('a.link').attr('href');
       links.push(link);
     });
 
